f681a8db770a655b5d786293cc3c9179e5039dc0
Ilia K <ISKalinin@alfabank.ru>
2018-11-07 14:23:47 +0300

    add loops and сconditions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var data = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 
 var appData = {
-    budjet : money,         // бюджет
+    budget : money,         // бюджет
     timeData : data,        // данные времени
     expenses : {} , // объект с обязательными расходами
     optionalExpenses : {},  // объект с необязательными расходами
@@ -11,13 +11,58 @@ var appData = {
     savings : false
 };
 
-let mandatoryExpenditure1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    cost1 = prompt("Во сколько обойдется?", ''),
-	mandatoryExpenditure2 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	cost2 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.mandatoryExpenditure1 = cost1;
-appData.expenses.mandatoryExpenditure2 = cost2;
+    
+    // for (i = 0; i < 2; i++) {
+    // let mandatoryExpenditure = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    //     cost = prompt("Во сколько обойдется?", '');
 
+    //     if ( (typeof(mandatoryExpenditure)) === 'string' && (typeof(mandatoryExpenditure)) != null && (typeof(cost)) != null 
+    //     && mandatoryExpenditure != '' && cost != null && mandatoryExpenditure.length < 50) {
+    //         appData.expenses[mandatoryExpenditure] = cost;
+    //     }
+    //     else {
+    //         alert('Введите данные еще раз!');
+    //         i--;
+    //     }
+    // }
 
-alert('Ваш бюджет на 1 день: ' + money/30);
+     //while (with out if)
+    // var i = 0;
+    // while (i < 2) {
+    //     let mandatoryExpenditure = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    //     cost = prompt("Во сколько обойдется?", '');
+
+    //     appData.expenses[mandatoryExpenditure] = cost;
+    //     i++
+    // }
+    
+
+    //do-while (with out if)
+    var i = 0;
+    do {
+    let mandatoryExpenditure = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        cost = prompt("Во сколько обойдется?", '');
+
+        appData.expenses[mandatoryExpenditure] = cost;
+        i++
+    }
+    while (i < 2);
+    
+
+appData.moneyPerDay = appData.budget / 30;        
+
+alert('Ваш бюджет на 1 день: ' + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('малоДенег');
+}
+else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('среднеДенег');
+}
+else if (appData.moneyPerDay > 2000) {
+    console.log('многоДенег');
+}
+else {
+    console.log('Что-то пошло не так...');
+}
